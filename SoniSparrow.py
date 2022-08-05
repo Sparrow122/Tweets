3abc299497b44d58ecaff0e2c3390e3deaef6b30
Saurav soni <98861920+Sparrow122@users.noreply.github.com>
2022-08-05 13:11:22 +0530

    Update SoniSparrow.py

--- SoniSparrow.py
+++ SoniSparrow.py
@@ -2,44 +2,51 @@
 
 import tweepy
 # current authentication Handler- OAuth 1.0a User Context authentication Hanadler
-class Tweets:
-    def __init__(self,username):
-        self.username=username
-    
+
+
+class GetTweets:
+    def __init__(self, username):
+        self.username = username
+
     def Tweets(self):
-        auth = tweepy.OAuth1UserHandler(
-            "api_key", "api_key_secret",
-            "access_token", "access_token_secret"
-        )
-        #api v1.1 ka client
-        client = tweepy.API(auth)
-        def Finduserid(username):
         try:
-            userid=client.get_user(screen_name=f"{username}")
-            dict1=userid._json
-            return (dict1['id'])
+            auth = tweepy.OAuth1UserHandler(
+                "api_key", "api_key_secret",
+                "access_token", "access_token_secret")
+            # api v1.1 ka client
+            client = tweepy.API(auth)
+            getusrid = client.get_user(screen_name=f"{self.username}")
+            dict1 = getusrid._json
+            userid = dict1['id']
 
+            # api v2 client
+            clientv2 = tweepy.Client(
+            bearer_token='bearer_token')
+            index = 1
+            for tweets in tweepy.Paginator(clientv2.get_users_tweets, id=f'{userid}'):
+                tweetdata = tweets[0]
+                for tweet in tweetdata:
+                    print(f"{index} | {tweet.id} | {tweet.text}")
+                    index += 1
+                    if (index == 50):
+                        break
+                break
         except Exception as err:
-            return (err)
-
-    username=input("username(screen_name): ")
-    userid=Finduserid(username)
-    print (userid)
-    #api v2 client
-    clientv2=tweepy.Client(bearer_token='bearer_token')
-    try:
-        #tweets=clientv2.get_users_tweets(id=f'{userid}',place_fields={'country' :'Taiwan'})
-        index=1
-        for tweets in tweepy.Paginator(clientv2.get_users_tweets,id=f'{userid}'):
-            tweetdata=tweets[0]
-            for tweet in tweetdata:
-                print (f"{index} | {tweet.id} | {tweet.text}")
-                index+=1
-    except Exception as err:
-        print (err)
+            print(err)
 
 
+Hindustaan = GetTweets('htTweets')
+Hindustaan.Tweets()
+print ()
+print ()
+Hindu = GetTweets('the_hindu')
+Hindu.Tweets()
+print ()
+print ()
+Timesofindia = GetTweets('timesofindia')
+Timesofindia.Tweets()
 
-#Hindustaan times= 36327407
-#The Hindu= 36327407
-#The Times of India= 134758540
+
+# Hindustaan times= 36327407
+# The Hindu= 36327407
+# The Times of India= 134758540
