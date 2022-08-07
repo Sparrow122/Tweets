39a887f5d0304c9c11ecb9708b8ac6fed26f8a44
Saurav soni <98861920+Sparrow122@users.noreply.github.com>
2022-08-07 18:20:26 +0530

    Update SoniSparrow.py

--- SoniSparrow.py
+++ SoniSparrow.py
@@ -1,52 +1,36 @@
-
-
 import tweepy
 # current authentication Handler- OAuth 1.0a User Context authentication Hanadler
 
 
-class GetTweets:
-    def __init__(self, username):
-        self.username = username
 
-    def Tweets(self):
-        try:
-            auth = tweepy.OAuth1UserHandler(
+def Getuserid(username):
+    auth = tweepy.OAuth1UserHandler(
                 "api_key", "api_key_secret",
                 "access_token", "access_token_secret")
-            # api v1.1 ka client
-            client = tweepy.API(auth)
-            getusrid = client.get_user(screen_name=f"{self.username}")
-            dict1 = getusrid._json
-            userid = dict1['id']
-
-            # api v2 client
-            clientv2 = tweepy.Client(
-            bearer_token='bearer_token')
-            index = 1
-            for tweets in tweepy.Paginator(clientv2.get_users_tweets, id=f'{userid}'):
-                tweetdata = tweets[0]
-                for tweet in tweetdata:
-                    print(f"{index} | {tweet.id} | {tweet.text}")
-                    index += 1
-                    if (index == 50):
-                        break
-                break
-        except Exception as err:
-            print(err)
+    # api v1.1 ka client
+    client = tweepy.API(auth)
+    getusrid = client.get_user(screen_name=f"{username}")
+    dict1 = getusrid._json
+    return dict1['id']
 
 
-Hindustaan = GetTweets('htTweets')
-Hindustaan.Tweets()
-print ()
-print ()
-Hindu = GetTweets('the_hindu')
-Hindu.Tweets()
-print ()
-print ()
-Timesofindia = GetTweets('timesofindia')
-Timesofindia.Tweets()
 
 
-# Hindustaan times= 36327407
-# The Hindu= 36327407
-# The Times of India= 134758540
+def Gettweets(username):
+    userid=Getuserid(username)
+    clientv2 = tweepy.Client(
+    bearer_token='bearer_token')
+    tweetscount = 1
+    try:
+        for tweets in tweepy.Paginator(clientv2.get_users_tweets, id=f'{userid}'):
+            tweetdata = tweets[0]
+            for tweet in tweetdata:
+                print(f"{tweetscount} | {tweet.id} | {tweet.text}")
+                tweetscount += 1
+                #if you want to set limit of tweets retriving uncomment the section below and set the integer value according to needs
+                #if (tweetscount == integer):
+                #    break
+            #if (tweetscount == integer):    
+            #    break
+    except Exception as err:
+        print(err)
