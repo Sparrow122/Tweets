2e1a2a925958756e9d56c0cf6b4d6dd017d0d269
Saurav soni <98861920+Sparrow122@users.noreply.github.com>
2022-08-08 08:13:16 +0530

    Update tweetsonhashtag.py

--- tweetsonhashtag.py
+++ tweetsonhashtag.py
@@ -6,6 +6,6 @@ client = tweepy.API(auth)
 
 for outputdict1 in tweepy.Cursor(client.search_tweets,q=['#LalSinghChadda']).items():
     dict2=outputdict1._json
-    if (dict2['user']['followers_count'] > 300):
-        print (f"text: {dict2['text']}  ||  tweeted_at: {dict2['created_at']}  ||  link: {dict2['user']['url']}")
+    if (dict2['user']['followers_count'] > 300 and dict2['retweet_count']>300):
+        print (f"text: {dict2['text']}  ||  tweeted_at: {dict2['created_at']}  ||  link: {dict2['user']['url']}  ||  retweeted: {dict2['retweet_count']} times  ||  likes: {dict2['retweeted_status']['favorite_count']}")
         print ()  
